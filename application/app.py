1ae97d22a14bf7f23ed5ecdaf311e0ef670a65ea
N4oto-T <h13.only57@gmail.com>
2024-01-03 18:23:05 +0900

    ファクトリの追加

---
+++ application/app.py
@@ -0,0 +1,19 @@
+"""
+Flaskアプリのインスタンス化、configの読み込み、blueprintの登録を行う。ファクトリの役割
+"""
+
+from flask import Flask
+
+from application.rest import room
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    config_module = f"application.config.{config_name.capitalize()}Config"
+
+    app.config.from_object(config_module)
+
+    app.register_blueprint(room.blueprint)
+
+    return app
